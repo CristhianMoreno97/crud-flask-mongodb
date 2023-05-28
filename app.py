363de1e80e271c7f609cccc46a4d7b2a92715493
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-28 15:34:49 -0500

    refactor

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from views import home_view
 
 app = Flask(__name__)
-app.register_blueprint(home_view.blueprint)
+app.register_blueprint(home_view.home_blueprint)
 
 def run_app():
     app.run(debug=True, port=4000)
--- views/home_view.py
+++ views/home_view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-blueprint = Blueprint('home', __name__, url_prefix='/')
+home_blueprint = Blueprint('home', __name__, url_prefix='/')
 
-@blueprint.route('/')
+@home_blueprint.route('/')
 def home() -> str:
     return render_template('index.html')
